b7563041b5497d4857fea332075409587ed495b0
Tactlesst <nasefaquiatan@gmail.com>
2025-07-27 21:38:30 +0800

    v9
    v9

--- pages/api/login.js
+++ pages/api/login.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const { email, password } = req.body;
 
   const client = new Client({
-    connectionString: process.env.NEON_DATABASE_URL,
+    connectionString: process.env.NETLIFY_DATABASE_URL,
   });
 
   try {
